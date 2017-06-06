f5bf2fb2fa4c0278e60d71468066835e31768f69
rcottrill <rob.meh@gmail.com>
2017-06-06 20:26:24 +0100

    Add files via upload

---
+++ Stop-AllVMs.ps1
@@ -0,0 +1,30 @@
+﻿
+workflow Stop-ALLVMs 
+{ 
+     
+    param( 
+ 
+      [string]$ResourceGroupName, 
+      [string]$TenantID, 
+      [string]$SPuser,
+      [string]$SPpass 
+     ) 
+     
+
+$ServicePassword = convertto-securestring $SPpass -asplaintext -force # The password of the Service Principal
+$creds = new-object -typename System.Management.Automation.PSCredential -argumentlist $SPuser, $ServicePassword
+Login-AzureRmAccount -Credential $creds -ServicePrincipal -TenantId $TenantID
+
+
+$GetVMs = get-AzureRmVM -ResourceGroupName "$ResourceGroupName"
+
+     if(!$GetVMs) { 
+        Write-Output "No VMs were found in your subscription." 
+     } else { 
+        Foreach ($VM in $GetVMs) 
+        { 
+            Write-Output "Stopping $($VM.Name)"
+        Stop-AzureRmVM -ResourceGroupName "$ResourceGroupName" -Name $VM.Name -Force -ErrorAction SilentlyContinue 
+        } 
+}
+}
