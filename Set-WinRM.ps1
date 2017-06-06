069356afaa8638a82313e94cf9e74cc14e4c8d61
rcottrill <rob.meh@gmail.com>
2017-06-06 20:17:49 +0100

    Add files via upload

---
+++ Set-WinRM.ps1
@@ -0,0 +1,84 @@
+﻿param
+(
+    [Parameter(Mandatory = $true)]
+    [string] $HostName
+)
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/201-vm-winrm-windows/ConfigureWinRM.ps1" -OutFile "$env:temp\ConfigureWinRM.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/201-vm-winrm-windows/winrmconf.cmd" -OutFile "$env:temp\winrmconf.cmd"
+New-Item c:\WinRM.txt -type file -Force
+
+
+function Delete-WinRMListener
+{
+    try
+    {
+        $config = Winrm enumerate winrm/config/listener
+        foreach($conf in $config)
+        {
+            if($conf.Contains("HTTPS"))
+            {
+                Write-Verbose "HTTPS is already configured. Deleting the exisiting configuration."
+    
+                winrm delete winrm/config/Listener?Address=*+Transport=HTTPS
+                break
+            }
+        }
+    }
+    catch
+    {
+        Write-Verbose -Verbose "Exception while deleting the listener: " + $_.Exception.Message
+    }
+}
+
+function Configure-WinRMHttpsListener
+{
+    param([string] $HostName
+       )
+
+    # Delete the WinRM Https listener if it is already configured
+    Delete-WinRMListener
+
+    # Create a test certificate
+    $thumbprint = (Get-ChildItem cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
+    if(-not $thumbprint)
+    {
+
+        $thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+
+        if(-not $thumbprint)
+        {
+            throw "Failed to create the test certificate."
+        }
+    }    
+
+    $response = cmd.exe /c $env:temp\winrmconf.cmd $hostname $thumbprint
+}
+
+function Add-FirewallException
+{
+    param([string] $port)
+
+    # Delete an exisitng rule
+    netsh advfirewall firewall delete rule name="Windows Remote Management (HTTPS-In)" dir=in protocol=TCP localport=$port
+
+    # Add a new firewall rule
+    netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=$port
+}
+
+
+#################################################################################################################################
+#                                              Configure WinRM                                                                  #
+#################################################################################################################################
+
+$winrmHttpsPort=5986
+
+# The default MaxEnvelopeSizekb on Windows Server is 500 Kb which is very less. It needs to be at 8192 Kb. The small envelop size if not changed
+# results in WS-Management service responding with error that the request size exceeded the configured MaxEnvelopeSize quota.
+winrm set winrm/config '@{MaxEnvelopeSizekb = "8192"}'
+
+# Configure https listener
+Configure-WinRMHttpsListener $HostName
+
+# Add firewall exception
+Add-FirewallException -port $winrmHttpsPort
